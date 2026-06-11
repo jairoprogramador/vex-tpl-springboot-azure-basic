25bed6b27e2e4c323ab60050c42be1fc43da2cd0
Jairo Galvez <jgalvez@baufest.com>
2026-06-10 23:27:02 -0500

    fix: standard_d2s_v3

--- steps/02-supply/terraform/main/variables.tf
+++ steps/02-supply/terraform/main/variables.tf
@@ -103,7 +103,7 @@ variable "azure_kubernetes_cluster_node_count" {
 variable "azure_kubernetes_cluster_vm_size" {
   description = "Define the size of the virtual machines for the nodes of the AKS cluster"
   type        = string
-  default     = "Standard_B2ms"
+  default     = "standard_d2s_v3"
 }
 
 variable "azure_dns_prefix_aks" {
